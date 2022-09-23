06dc1d85f33fc2466aeacb3f17f4c4a071bc64c2
amruta shinde <105345456+Amuba4@users.noreply.github.com>
2022-09-23 13:15:46 +0530

    react-simple-props

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./componants/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title="Mywebsite"/>
+    </>
+  );
+}
+
+export default App;
